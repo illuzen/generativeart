f5da6407e5e3c0b68d4edb52af3b41aafdcdd3d5
illuzen <snakecharmr1024@gmail.com>
2021-01-31 09:10:13 -0600

    trying to fix the incomplete background paint issue

--- R/generate_plot.R
+++ R/generate_plot.R
@@ -4,7 +4,7 @@
 #' @param df the data frame created with `generate_data()`
 #' @param file_name the file name for saving the plot
 #' @param polar do you want to use a polar coordinate system? The default is a cartesian coordinate system.
-#' @param file_name filetype of the final image. Default is `png`, for other options see the `devics` argument in `gggplot::gsave()`
+#' @param file_name filetype of the final image. Default is `png`, for other options see the `devics` argument in `ggplot::gsave()`
 #' @param color color of the points. default is black
 #' @param background_color background color of the plaut. default is white.
 #' @param filetype set the file type for the image
@@ -23,22 +23,22 @@ generate_plot <- function(df, file_name, polar, filetype, color = "black", backg
       ggplot2::ggplot(ggplot2::aes(x = x, y = y)) +
       ggplot2::geom_point(alpha = 0.1, size = 0, shape = 20, color = color) +
       ggplot2::theme_void() +
-      ggplot2::coord_fixed() +
-      ggplot2::coord_polar() +
       ggplot2::theme(
         panel.background = element_rect(fill = background_color),
         plot.background = element_rect(fill = background_color)
-        )
+        ) +
+      ggplot2::coord_fixed() +
+      ggplot2::coord_polar()
   } else {
     plot <- df %>%
       ggplot2::ggplot(ggplot2::aes(x = x, y = y)) +
       ggplot2::geom_point(alpha = 0.1, size = 0, shape = 20, color = color) +
       ggplot2::theme_void() +
-      ggplot2::coord_fixed() +
       ggplot2::theme(
         panel.background = element_rect(fill = background_color),
         plot.background = element_rect(fill = background_color)
-        )
+        ) +
+      ggplot2::coord_fixed()
   }
   ggplot2::ggsave(plot, filename = paste0(IMG_PATH, file_name), width = 6, height = 6, device = filetype)
   print("image saved...")
